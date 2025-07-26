dff288aca9c80a31c93fcb5fa02e666ef86c308e
DatzProject <hardilol92@gmail.com>
2025-07-26 21:26:48 +0800

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3390,7 +3390,7 @@ const SplashScreen: React.FC = () => {
         className="w-52 h-70 mb-4 animate-pulse-custom" //Pengaturan ukuran logo
       />
       <p className="text-gray-800 text-lg font-semibold mt-6">
-        Tunggu Sebentar
+        Aisyah, S.Pd.SD
       </p>
     </div>
   );
